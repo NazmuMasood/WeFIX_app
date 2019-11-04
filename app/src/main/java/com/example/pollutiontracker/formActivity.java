888a186978d9a5913fa9fa4817d793347d0ac112
NazmuMasood <44304205+NazmuMasood@users.noreply.github.com>
2019-11-04 21:57:37 +0600

    formActivity - onClick implemented and long-press to remove pick-image intent, multiple select working now

--- app/src/main/java/com/example/pollutiontracker/formActivity.java
+++ app/src/main/java/com/example/pollutiontracker/formActivity.java
@@ -28,7 +28,9 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class formActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,
-        View.OnLongClickListener {
+        View.OnLongClickListener,
+        View.OnClickListener
+{
 
     EditText locationET;
     Spinner categorySpinner, sourceSpinner, extentSpinner;
@@ -107,6 +109,7 @@ public class formActivity extends AppCompatActivity implements AdapterView.OnIte
     private void openFileChooser() {
         Intent intent = new Intent();
         intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
@@ -116,40 +119,82 @@ public class formActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null){
+                && data != null){
 
-            mImageUri = data.getData();
+            //If user selects multiple images
+            if(data.getClipData() != null) {
+                int countClipData = data.getClipData().getItemCount();
+                int currentImgSelect = 0;
+                while (currentImgSelect < countClipData) {
 
-            try {
-                Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageUri);
-                int nh = (int) ( bitmapImage.getHeight() * (512.0 / bitmapImage.getWidth()) );
-                Bitmap scaled = Bitmap.createScaledBitmap(bitmapImage, 512, nh, true);
+                    mImageUri = data.getClipData().getItemAt(currentImgSelect).getUri();
 
-                ImageView tempImg = new ImageView(this);
-                tempImg.setImageBitmap(scaled);
-                long time = new Date().getTime();
+                    try {
+                        Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageUri);
+                        int nh = (int) (bitmapImage.getHeight() * (512.0 / bitmapImage.getWidth()));
+                        Bitmap scaled = Bitmap.createScaledBitmap(bitmapImage, 512, nh, true);
 
-                int mWidth = imgIB.getWidth();
-                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                        mWidth,
-                        LinearLayout.LayoutParams.MATCH_PARENT);
-                lp.setMarginStart(10);
-                tempImg.setLayoutParams(lp);
-                tempImg.setTag("IMG_"+time);
-                imgLL.addView(tempImg);
+                        ImageView tempImg = new ImageView(this);
+                        tempImg.setImageBitmap(scaled);
+                        long time = new Date().getTime();
 
-                images.put("IMG_"+time, mImageUri);
-                tempImg.setOnLongClickListener(this);
-            }
-            catch (Exception e){e.printStackTrace();}
+                        int mWidth = imgIB.getWidth();
+                        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                                mWidth,
+                                LinearLayout.LayoutParams.MATCH_PARENT);
+                        lp.setMarginStart(10);
+                        tempImg.setLayoutParams(lp);
+                        tempImg.setTag("IMG_" + time);
+                        imgLL.addView(tempImg);
+
+                        images.put("IMG_" + time, mImageUri);
+                        tempImg.setOnLongClickListener(this);
+                        tempImg.setOnClickListener(this);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    currentImgSelect++;
+                }
+            }// if [ data.getClipData() ] ends
+
+            //If user selects a single image
+            else if (data.getData() != null){
+
+                mImageUri = data.getData();
+
+                try {
+                    Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageUri);
+                    int nh = (int) (bitmapImage.getHeight() * (512.0 / bitmapImage.getWidth()));
+                    Bitmap scaled = Bitmap.createScaledBitmap(bitmapImage, 512, nh, true);
+
+                    ImageView tempImg = new ImageView(this);
+                    tempImg.setImageBitmap(scaled);
+                    long time = new Date().getTime();
+
+                    int mWidth = imgIB.getWidth();
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                            mWidth,
+                            LinearLayout.LayoutParams.MATCH_PARENT);
+                    lp.setMarginStart(10);
+                    tempImg.setLayoutParams(lp);
+                    tempImg.setTag("IMG_" + time);
+                    imgLL.addView(tempImg);
+
+                    images.put("IMG_" + time, mImageUri);
+                    tempImg.setOnLongClickListener(this);
+                    tempImg.setOnClickListener(this);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }// else if [ data.getData() ] ends
         }
-    }
+    }//onActivityResult ends
 
     @Override
-    public boolean onLongClick(View v) {
-        //return false;
+    public void onClick(View v) {
         ImageView imageView = imgLL.findViewWithTag(v.getTag());
-        toaster.shortToast("You clicked image : "+imageView.getTag(), formActivity.this);
+        toaster.longToast("You clicked image : "+imageView.getTag(), formActivity.this);
 
         String key = (String) imageView.getTag();
         Uri thisImageUri = images.get(key);
@@ -158,6 +203,17 @@ public class formActivity extends AppCompatActivity implements AdapterView.OnIte
         Intent intent = new Intent(this, imgActivity.class);
         intent.putExtra("ImageUri", thisImageUri);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        //return false;
+        ImageView imageView = imgLL.findViewWithTag(v.getTag());
+        toaster.longToast("You long-pressed image : "+imageView.getTag(), formActivity.this);
+
+        String key = (String) imageView.getTag();
+        images.remove(key);
+        imgLL.removeView(v);
 
         return true;
     }
